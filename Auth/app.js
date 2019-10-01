e0915572da0085b479558ddd395cae3d859f4b2c
Brogan Miner <BroganMiner22@gmail.com>
2019-10-01 13:37:02 -0700

    move from cas-dev

--- Auth/app.js
+++ Auth/app.js
@@ -26,7 +26,7 @@ exports.login = async (event, context) => {
     return response.redirect(cookie.parse(returnURICookie)['redirect'])
   } else {
     response.updateCookie(returnURICookie)
-    return response.redirect('https://login.oregonstate.edu/cas-dev/login?service=https://api.sustainability.oregonstate.edu/v2/auth/session')
+    return response.redirect('https://login.oregonstate.edu/cas/login?service=https://api.sustainability.oregonstate.edu/v2/auth/session')
   }
 }
 
@@ -42,24 +42,23 @@ exports.logout = async (event, context) => {
   response.updateCookie(cookie.serialize('token', 'invalid', {
     expires: new Date(0)
   }))
-  return response.redirect('https://login.oregonstate.edu/idp/profile/cas-dev/logout')
+  return response.redirect('https://login.oregonstate.edu/idp/profile/cas/logout')
 }
 
 exports.session = async (event, context) => {
   const validation = await axios('https://login.oregonstate.edu/idp/profile/cas/serviceValidate?ticket=' + event.queryStringParameters.ticket + '&service=https://api.sustainability.oregonstate.edu/v2/auth/session')
   let response = new Response()
-  response.body = validation.data
-  // if (validation.status === 200) {
-  //   const parser = new DomParser()
-  //   const body = parser.parseFromString(validation.data)
-  //   let JSONRep = {
-  //     onid: body.getElementsByTagName('cas:cas')[0].childNodes[0].textContent,
-  //     firstName: body.getElementsByTagName('cas:firstname')[0].childNodes[0].textContent,
-  //     primaryAfiliation: body.getElementsByTagName('cas:eduPersonPrimaryAffiliation')[0].childNodes[0].textContent
-  //   }
-  //   // eslint-disable-next-line no-new
-  //   new User(JSONRep, response)
-  //   return response.redirect(cookie.parse(event.headers.Cookie).redirect)
-  // }
+  if (validation.status === 200) {
+    const parser = new DomParser()
+    const body = parser.parseFromString(validation.data)
+    let JSONRep = {
+      onid: body.getElementsByTagName('cas:cas')[0].childNodes[0].textContent,
+      firstName: body.getElementsByTagName('cas:firstname')[0].childNodes[0].textContent,
+      primaryAfiliation: body.getElementsByTagName('cas:eduPersonPrimaryAffiliation')[0].childNodes[0].textContent
+    }
+    // eslint-disable-next-line no-new
+    new User(JSONRep, response)
+    return response.redirect(cookie.parse(event.headers.Cookie).redirect)
+  }
   return response
 }
