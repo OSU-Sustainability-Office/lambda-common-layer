38353e7f53bf72466e4c0330365a955335b03e52
Brogan Miner <BroganMiner22@gmail.com>
2019-10-02 13:02:40 -0700

    cap s

--- Auth/app.js
+++ Auth/app.js
@@ -10,8 +10,8 @@ const Response = require('/opt/nodejs/response.js')
 const axios = require('axios')
 const DomParser = require('dom-parser')
 const cookie = require('cookie')
-const jwt = require('jsonwebtoken')
-require('dotenv').config({ path: '/opt/nodejs/.env' })
+// const jwt = require('jsonwebtoken')
+// require('dotenv').config({ path: '/opt/nodejs/.env' })
 
 exports.login = async (event, context) => {
   let returnURICookie
@@ -57,7 +57,7 @@ exports.session = async (event, context) => {
   const validation = await axios('https://login.oregonstate.edu/idp/profile/cas/serviceValidate?ticket=' + event.queryStringParameters.ticket + '&service=https://api.sustainability.oregonstate.edu/v2/auth/session')
   let response = new Response()
   if (validation.status === 200) {
-    if (validation.data.includes('success')) {
+    if (validation.data.includes('Success')) {
       const parser = new DomParser()
       const body = parser.parseFromString(validation.data)
       let JSONRep = {
@@ -68,9 +68,10 @@ exports.session = async (event, context) => {
       // eslint-disable-next-line no-new
       let user = new User(JSONRep, response)
       await user.resolved
-      response.updateCookie(cookie.serialize('token', jwt.sign(user.data, process.env.JWT_KEY), { httpOnly: false, secure: false }))
+      response.body = 'test'
+      // response.updateCookie(cookie.serialize('token', jwt.sign(user.data, process.env.JWT_KEY), { httpOnly: false, secure: false }))
     }
-    response.body = validation.data
+    response.body = JSON.stringify(response)
     return response
     // return response.redirect(cookie.parse(event.headers.Cookie).redirect)
   }
