e021f1a2c1db515b2807c44fe96fe332447e90dc
kristinaxlee <50277029+kristinaxlee@users.noreply.github.com>
2020-12-28 14:49:36 -0800

    added CORS to Response class
    (pulled back old code from before)
    CORS issue when trying to log in & can't run lambda common layer locally, so trying to fix this way

--- dependencies/nodejs/response.js
+++ dependencies/nodejs/response.js
@@ -1,17 +1,27 @@
-/*
- * Created Date: Monday, May 13th 2019, 7:15:01 pm
- * Author: Brogan
- * Copyright (c) 2019 Oregon State University
- */
 require('dotenv').config({ path: '/opt/nodejs/.env' })
 
 class Response {
   constructor (event) {
+    if (event && event.headers.origin) {
+      this.headers = {
+        'Access-Control-Allow-Origin': event.headers.origin,
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    } else if (event && event.headers.referer) {
+      this.headers = {
+        'Access-Control-Allow-Origin': event.headers.referer,
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    } else {
+      this.headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true'
+      }
+    }
+
     this.statusCode = 200
     this.body = ''
-    this.headers = {
-      
-    }
+    
   }
   updateCookie (cookie) {
     this.headers['Set-Cookie'] = cookie + '; Path=/v2; Domain=.oregonstate.edu; Max-Age=7200; SameSite=None; Secure' // This prevents the path from defaulting to /v2/auth on Firefox, and makes the cookie expire in 2 hours.
